453607583805c342f7ce338dd3e1d03dccb52830
senneverlinden <58658195+senneverlinden@users.noreply.github.com>
2025-12-07 02:08:14 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,14 @@
 // Jaar automatisch invullen in footer
 document.getElementById('year').textContent = new Date().getFullYear();
 
-// Kaart maken voor Boom
-var map = L.map('map').setView([51.0897, 4.3706], 13);
-
-// Tegels toevoegen aan de kaart
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19
-}).addTo(map);
-
-// Marker op Boom zetten
-var marker = L.marker([51.0897, 4.3706]).addTo(map);
-marker.bindPopup('<b>Boom, Antwerpen</b><br>Hier ben ik gevestigd!').openPopup();
+// Check of Leaflet bestaat
+if (typeof L !== 'undefined') {
+  var map = L.map('map').setView([51.0897, 4.3706], 13);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+  }).addTo(map);
+  var marker = L.marker([51.0897, 4.3706]).addTo(map);
+  marker.bindPopup('<b>Boom, Antwerpen</b><br>Hier ben ik gevestigd!').openPopup();
+} else {
+  console.log('Leaflet niet geladen!');
+}
